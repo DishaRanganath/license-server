ffffd53c2c0f75558ac20faa47dc5eec1d61dcbf
DishaRanganath <disharanganath21@gmail.com>
2026-04-20 21:07:30 +0530

    modifications for resetting devices

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 require('./src/config/db');
 
 const app = require('./src/app');
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 10000;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
